74fd21c40aa4b3c8782c14a9393d43c60822b12d
vki <vki@tiqri.com>
2018-06-05 10:56:45 +0530

    Qr-code image added.

--- app/src/main/java/com/example/vki/test2/ThirdActivity.java
+++ app/src/main/java/com/example/vki/test2/ThirdActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ThirdActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class ThirdActivity extends AppCompatActivity {
     Button successButton;
     Button failButton;
     TextView message;
+    ImageView qrCodeImaView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +22,15 @@ public class ThirdActivity extends AppCompatActivity {
         successButton = findViewById(R.id.btn_success);
         failButton = findViewById(R.id.btn_Fail);
         message = findViewById(R.id.tv_msg2);
+        qrCodeImaView = findViewById(R.id.img_view1);
         this.getSupportActionBar().setTitle("DEV DAY-2018");
+        qrCodeImaView.setVisibility(View.INVISIBLE);
 
         successButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 message.setText("Great! We have done your reservation.\nThis is your ID: DVD18-0001 \nSee you there...!");
+                qrCodeImaView.setVisibility(View.VISIBLE);
                 message.setTextColor(Color.GREEN);
             }
         });
