75a30b16bb725251aea071ccd3dd2ee60be20c19
vki <vki@tiqri.com>
2018-06-06 15:19:39 +0530

    Grid is completed.

--- app/src/main/java/com/example/vki/test2/FourthActivity.java
+++ app/src/main/java/com/example/vki/test2/FourthActivity.java
@@ -2,12 +2,26 @@ package com.example.vki.test2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class FourthActivity extends AppCompatActivity {
+
+    ImageView sessionInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getSupportActionBar().setTitle("DEV DAY-2018");
         setContentView(R.layout.activity_fourth);
+        sessionInfo = findViewById(R.id.imgv_sessioninfo);
+
+        sessionInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FourthActivity.this, "Elaaa", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
